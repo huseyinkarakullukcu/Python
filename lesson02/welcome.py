0e16aac68676af894991b1a2d55edef935764621
huseyinkarakullukcu <huseyinkarakullukcu52@gmail.com>
2024-07-02 23:54:32 +0300

    up

---
+++ lesson02/welcome.py
@@ -0,0 +1,16 @@
+'''
+Python Basics
+'''
+
+line01 = "********************" #header / footer
+line02 = "*                  *" #re-use
+line03 = "*     WELCOME!     *"
+
+print("") #starts with a blank line
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+#diğer programlama dilleri kod yazarken bırakılan boşlukları dikkate almaz fakat python alır
---
+++ lesson03/operators.py
@@ -0,0 +1,41 @@
+#atama
+name = "Jon"
+
+# +,-,*,/ aritmatik işlemler
+
+# //-bölüm kısmını verir, %-kalanı verir,  **-üst alma
+number = 5
+number +=1 #arttırma yapar, +,-,*,/
+#round(number) - tamkısmı alır
+
+full_name = "John " + "Doe" 
+
+42 == 41
+
+42 != 41
+
+10 > 5
+10 < 5
+
+10 >= 5
+10 <= 5
+
+x = True
+y = False
+
+x = not x
+
+x and y
+x or y
+
+
+meaning = 42
+print('')
+
+# if meaning > 10:
+#     print('Right on!')
+# else:
+#     print("Not Today")
+
+#tek satırda yazma - Ternary Operator
+print('Right on!') if meaning > 10 else print('Not Today')
